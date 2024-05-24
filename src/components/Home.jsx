dfc82fb4de6272ad92bdf55ffaefbdd8e4edd436
Sugambaj7 <sugambaj123@gmail.com>
2024-05-24 10:52:33 +0545

    made home responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,47 +8,47 @@ import "../assets/css/Home.css";
 
 const Home = () => {
   return (
-    <div className="mt-28 ">
-      <div className="flex flex-col justify-center items-center">
-        <div>
-          <div className="w-24 h-24 rounded-full p-1 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
-            <img
-              src={avatar}
-              alt="myself"
-              className="h-full w-full object-cover rounded-full "
-            />
-          </div>
+    <div className="mt-16 ml-12 mr-2 mb-10 flex flex-col justify-center items-center sm:ml-24 sm:mr-10 lg:mt-28">
+      <div className="w-full flex justify-center">
+        <div className="w-24 h-24 rounded-full p-1 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] sm:w-28 sm:h-28 lg:w-36 lg:h-36">
+          <img
+            src={avatar}
+            alt="myself"
+            className="h-full w-full object-cover rounded-full "
+          />
         </div>
-        <h1 className="mt-4 text-2xl font-semibold">Sugam Bajracharya</h1>
-        <p className="mb-2 text-md">I'm a Student</p>
-        <div className="mb-6">
-          <ul className="flex flex-row space-x-4 text-xl">
-            <li>
-              <a href="https://www.instagram.com/sugam_bajracharya/">
-                <IoLogoInstagram />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/SugamBajra">
-                <FaFacebook />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/sugam-bajracharya-227b70210">
-                <FaLinkedin />
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <FaXTwitter />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <button className="bg-black py-1 px-3 rounded text-white">
-          Hire Me
-        </button>
       </div>
+      <h1 className="text-center text-2xl font-semibold mb-1 mt-4 sm:text-2xl md:text-3xl lg:text-4xl">
+        Sugam Bajracharya
+      </h1>
+      <p className="mb-3 text-xl text-center md:text-2xl mt-1 lg:3xl lg:mt-2">
+        I'm a Student
+      </p>
+      <div className="mb-6 md:mt-2 lg:mt-3">
+        <ul className="flex flex-row text-xl space-x-2 md:text-2xl lg:space-x-3 lg:text-3xl">
+          <li>
+            <a href="https://www.instagram.com/sugam_bajracharya/">
+              <IoLogoInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/SugamBajra">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/sugam-bajracharya-227b70210">
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <FaXTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <button className="bg-black py-1 px-3 rounded text-white">Hire Me</button>
     </div>
   );
 };
