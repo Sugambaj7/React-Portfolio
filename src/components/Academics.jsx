bd1e2642912f1e80ba4a08ffe96c08251efc3081
Sugambaj7 <sugambaj123@gmail.com>
2024-05-24 14:39:44 +0545

    made academics responsive and fixed previous issues in it

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -5,13 +5,13 @@ import { LuGraduationCap } from "react-icons/lu";
 
 const Academics = () => {
   return (
-    <section>
-      <h2 className="flex justify-center items-center text-3xl mb-14 font-bold">
+    <div className="ml-12 mr-2 flex flex-col sm:ml-24">
+      <h2 className="text-center text-3xl font-bold mb-8 lg:text-4xl">
         Academics & Experience
       </h2>
 
-      <div className="flex flex-col md:flex-row justify-center items-stretch mb-8 ">
-        <div className="w-full m-3 md:w-[33%] academics__timeline  p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg relative mr-10 flex flex-col justify-center items-stretch">
+      <div className="flex flex-col ml-6 mr-6 md:flex-row ">
+        <div className="academics__timeline shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg relative flex flex-col pl-6 pr-8 sm:pl-20 md:flex-1 md:mt-4 md:mb-10">
           {AcademicsData.map((list, id) => {
             if (list.category == "Academics") {
               return (
@@ -27,7 +27,7 @@ const Academics = () => {
             }
           })}
         </div>
-        <div className="w-full m-3 md:w-[33%] experience__timeline p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg relative">
+        <div className="experience__timeline mt-4 mb-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg relative flex flex-col pl-6 pr-8 sm:pl-20 md:flex-1 md:ml-4">
           {AcademicsData.map((list, id) => {
             if (list.category == "Experience") {
               return (
@@ -44,7 +44,7 @@ const Academics = () => {
           })}
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,15 @@ import "../assets/css/Card.css";
 
 const Card = (props) => {
   return (
-    <div className="timeline_item relative pb-14 pl-8 leading-8">
+    <div className="timeline_item relative pb-7 pl-8 leading-7">
       <div className="flex flex-row">
-        <span className="timeline-icon text-2xl">{props.icon}</span>
-        <span className="timeline__date">{props.year}</span>
+        <span className="timeline-icon text-2xl sm:text-4xl">{props.icon}</span>
+        <span className="timeline__date text-md sm:text-xl">{props.year}</span>
       </div>
-      <h3 className="timeline__title font-bold text-lg">{props.title}</h3>
-      <p className="timeline__desc">{props.description}</p>
+      <h3 className="timeline__title font-bold text-lg sm:text-xl">
+        {props.title}
+      </h3>
+      <p className="timeline__desc mt-2 sm:text-lg">{props.description}</p>
     </div>
   );
 };
